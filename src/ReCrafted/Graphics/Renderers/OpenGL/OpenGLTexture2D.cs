444934bdc6a6105f35c1d889d5f085ef225cddf3
Damian Korczowski <erdroy2@gmail.com>
2017-01-14 14:24:00 +0100

    texture loading

--- src/ReCrafted/Graphics/Renderers/OpenGL/OpenGLTexture2D.cs
+++ src/ReCrafted/Graphics/Renderers/OpenGL/OpenGLTexture2D.cs
@@ -1,6 +1,9 @@
 ﻿// ReCrafted © 2016-2017 Damian 'Erdroy' Korczowski
 
 using System.Drawing;
+using System.Drawing.Imaging;
+using OpenTK.Graphics.OpenGL;
+using ReCrafted.Graphics.Renderers.OpenGL;
 
 namespace ReCrafted.Graphics.Renderers.OpenGLMesh
 {
@@ -18,13 +21,34 @@ namespace ReCrafted.Graphics.Renderers.OpenGLMesh
         /// <param name="maxMips">The maximal count of mipmaps.</param>
         protected override void Load(Bitmap bitmap, bool genMips, int maxMips)
         {
+            int texture;
+            GL.GenTextures(1, out texture);
+            GL.BindTexture(TextureTarget.Texture2D, texture);
+
+            var data = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0,  OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+            bitmap.UnlockBits(data);
+            
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
+
+            Texture = texture;
+            GL.BindTexture(TextureTarget.Texture2D, 0);
+
+            OpenGLRenderer.CheckError();
         }
-        
+
         /// <summary>
         /// Dispose the texture.
         /// </summary>
         public override void Dispose()
         {
+            if(Texture >= 0)
+                GL.DeleteTexture(Texture);
         }
+
+        public int Texture { get; private set; } = -1;
     }
 }
