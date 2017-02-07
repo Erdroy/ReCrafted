4f293dd00b5778fab63c41031ce2c71c2b8b5ee9
Damian Korczowski <erdroy2@gmail.com>
2017-02-07 16:37:48 +0100

    added math library

---
+++ src/ReCrafted/Core/Math.h
@@ -0,0 +1,12 @@
+// ReCrafted © 2016-2017 Damian 'Erdroy' Korczowski and Mateusz 'Maturas' Zawistowski
+
+#pragma once
+
+#ifndef MATH_H
+#define MATH_H
+
+// includes
+#include <cml/cml.h>
+using namespace cml;
+
+#endif // MATH_H
