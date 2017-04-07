04978b0a28b292e4730e1fce5f3b4386146f6340
Damian Korczowski <erdroy2@gmail.com>
2017-04-07 20:40:03 +0200

    done ao

--- src/ReCrafted/Game/Voxels/VoxelChunk.cpp
+++ src/ReCrafted/Game/Voxels/VoxelChunk.cpp
@@ -91,6 +91,14 @@ FORCEINLINE void build_face(
 	}
 }
 
+FORCEINLINE float calcAO(bool sideA, bool sideB, bool corner)
+{
+	if (sideA && sideB)
+		return 1.0f;
+
+	return (sideA ? 0.5f : 0) + (sideB ? 0.5f : 0) + (corner ? 0.5f : 0);
+}
+
 void VoxelChunk::generateVoxelData() // WARNING: this should be run in job queue!
 {
 	// create voxels
@@ -132,6 +140,16 @@ void VoxelChunk::generateMesh(
 	const auto vec3_up = Vector3(0.0f, 1.0f, 0.0f);
 	const auto vec3_forward = Vector3(0.0f, 0.0f, 1.0f);
 
+	const auto ambientStr = 0.45f;
+
+	auto block_tf  = false, block_tb  = false, block_tl  = false, block_tr  = false;
+	auto block_tfl = false, block_tfr = false, block_tbl = false, block_tbr = false;
+	auto block_mfl = false, block_mfr = false, block_mbl = false, block_mbr = false;
+	auto block_bf  = false, block_bb  = false, block_bl  = false, block_br  = false;
+	auto block_bfl = false, block_bfr = false, block_bbl = false, block_bbr = false;
+
+	float ao00, ao01, ao11, ao10;
+
 	for (auto y = 0; y < ChunkHeight; y++)
 	{
 		for (auto x = 0; x < ChunkWidth; x++)
@@ -143,29 +161,148 @@ void VoxelChunk::generateMesh(
 
 				auto origin = Vector3(float(x), float(y), float(z));
 
+				auto leftFace = !getVoxelCC(x - 1, y, z);
+				auto rightFace = !getVoxelCC(x + 1, y, z);
+				auto bottomFace = !getVoxelCC(x, y - 1, z);
+				auto upperFace = !getVoxelCC(x, y + 1, z);
+				auto backFace = !getVoxelCC(x, y, z - 1);
+				auto frontFace = !getVoxelCC(x, y, z + 1);
+
+				if(leftFace || rightFace || bottomFace || upperFace || backFace || frontFace)
+				{
+					block_tf = getVoxelCC(x, y + 1, z + 1) != voxel_air;
+					block_tb = getVoxelCC(x, y + 1, z - 1) != voxel_air;
+					block_tl = getVoxelCC(x - 1, y + 1, z) != voxel_air;
+					block_tr = getVoxelCC(x + 1, y + 1, z) != voxel_air;
+
+					block_tfl = getVoxelCC(x - 1, y + 1, z + 1) != voxel_air;
+					block_tfr = getVoxelCC(x + 1, y + 1, z + 1) != voxel_air;
+					block_tbl = getVoxelCC(x - 1, y + 1, z - 1) != voxel_air;
+					block_tbr = getVoxelCC(x + 1, y + 1, z - 1) != voxel_air;
+
+					block_mfl = getVoxelCC(x - 1, y, z + 1) != voxel_air;
+					block_mfr = getVoxelCC(x + 1, y, z + 1) != voxel_air;
+					block_mbl = getVoxelCC(x - 1, y, z - 1) != voxel_air;
+					block_mbr = getVoxelCC(x + 1, y, z - 1) != voxel_air;
+
+					block_bf = getVoxelCC(x, y - 1, z + 1) != voxel_air;
+					block_bb = getVoxelCC(x, y - 1, z - 1) != voxel_air;
+					block_bl = getVoxelCC(x - 1, y - 1, z) != voxel_air;
+					block_br = getVoxelCC(x + 1, y - 1, z) != voxel_air;
+
+					block_bfl = getVoxelCC(x - 1, y - 1, z + 1) != voxel_air;
+					block_bfr = getVoxelCC(x + 1, y - 1, z + 1) != voxel_air;
+					block_bbl = getVoxelCC(x - 1, y - 1, z - 1) != voxel_air;
+					block_bbr = getVoxelCC(x + 1, y - 1, z - 1) != voxel_air;
+				}
+
 				// left face
-				if (!getVoxelCC(x - 1, y, z))
-					build_face(origin, vec3_up, vec3_forward, false, false, vertices_ptr, normals_ptr, uvs_ptr, indices_ptr);
+				if (leftFace)
+				{
+					ao00 = calcAO(block_mfl, block_bl, block_bfl);
+					ao01 = calcAO(block_mfl, block_tl, block_tfl);
+					ao11 = calcAO(block_mbl, block_tl, block_tbl);
+					ao10 = calcAO(block_mbl, block_br, block_bbl);
+
+					auto flippedQuad = ao01 + ao10 > ao00 + ao11;
+
+					build_face(origin, vec3_up, vec3_forward, false, flippedQuad, vertices_ptr, normals_ptr, uvs_ptr, indices_ptr);
+
+					colors_ptr->push_back({ ao10 * ambientStr, 0.0f, 0.0f, 0.0f });
+					colors_ptr->push_back({ ao11 * ambientStr, 0.0f, 0.0f, 0.0f });
+					colors_ptr->push_back({ ao01 * ambientStr, 0.0f, 0.0f, 0.0f });
+					colors_ptr->push_back({ ao00 * ambientStr, 0.0f, 0.0f, 0.0f });
+				}
 
 				// right face
-				if (!getVoxelCC(x + 1, y, z))
-					build_face(origin + vec3_right, vec3_up, vec3_forward, true, false, vertices_ptr, normals_ptr, uvs_ptr, indices_ptr);
+				if (rightFace)
+				{
+					ao00 = calcAO(block_br, block_mbr, block_bbr);
+					ao01 = calcAO(block_tr, block_mbr, block_tbr);
+					ao11 = calcAO(block_tr, block_mfr, block_tfr);
+					ao10 = calcAO(block_br, block_mfr, block_bfr);
+
+					auto flippedQuad = ao00 + ao11 > ao01 + ao10;
+
+					build_face(origin + vec3_right, vec3_up, vec3_forward, true, flippedQuad, vertices_ptr, normals_ptr, uvs_ptr, indices_ptr);
+
+					colors_ptr->push_back({ ao00 * ambientStr, 0.0f, 0.0f, 0.0f });
+					colors_ptr->push_back({ ao01 * ambientStr, 0.0f, 0.0f, 0.0f });
+					colors_ptr->push_back({ ao11 * ambientStr, 0.0f, 0.0f, 0.0f });
+					colors_ptr->push_back({ ao10 * ambientStr, 0.0f, 0.0f, 0.0f });
+				}
 
 				// upper face
-				if (!getVoxelCC(x, y + 1, z))
-					build_face(origin + vec3_up, vec3_forward, vec3_right, true, false, vertices_ptr, normals_ptr, uvs_ptr, indices_ptr);
+				if (upperFace)
+				{
+					ao00 = calcAO(block_tl, block_tb, block_tbl);
+					ao01 = calcAO(block_tl, block_tf, block_tfl);
+					ao11 = calcAO(block_tr, block_tf, block_tfr);
+					ao10 = calcAO(block_tb, block_tr, block_tbr);
+
+					auto flippedQuad = ao00 + ao11 > ao01 + ao10;
+
+					build_face(origin + vec3_up, vec3_forward, vec3_right, true, flippedQuad, vertices_ptr, normals_ptr, uvs_ptr, indices_ptr);
+
+					colors_ptr->push_back({ ao00 * ambientStr, 0.0f, 0.0f, 0.0f });
+					colors_ptr->push_back({ ao01 * ambientStr, 0.0f, 0.0f, 0.0f });
+					colors_ptr->push_back({ ao11 * ambientStr, 0.0f, 0.0f, 0.0f });
+					colors_ptr->push_back({ ao10 * ambientStr, 0.0f, 0.0f, 0.0f });
+				}
 
 				// bottom face
-				if (!getVoxelCC(x, y - 1, z) && y - 1 >= 0)
-					build_face(origin, vec3_forward, vec3_right, false, false, vertices_ptr, normals_ptr, uvs_ptr, indices_ptr);
+				if (bottomFace && y - 1 >= 0)
+				{
+					ao00 = calcAO(block_bb, block_bl, block_bbl);
+					ao01 = calcAO(block_bf, block_bl, block_bfl);
+					ao11 = calcAO(block_bf, block_br, block_bfr);
+					ao10 = calcAO(block_bb, block_br, block_bbr);
+
+					auto flippedQuad = ao00 + ao11 > ao01 + ao10;
+
+					build_face(origin, vec3_forward, vec3_right, false, flippedQuad, vertices_ptr, normals_ptr, uvs_ptr, indices_ptr);
+
+					colors_ptr->push_back({ ao00 * ambientStr, 0.0f, 0.0f, 0.0f });
+					colors_ptr->push_back({ ao01 * ambientStr, 0.0f, 0.0f, 0.0f });
+					colors_ptr->push_back({ ao11 * ambientStr, 0.0f, 0.0f, 0.0f });
+					colors_ptr->push_back({ ao10 * ambientStr, 0.0f, 0.0f, 0.0f });
+				}
 
 				// front face
-				if (!getVoxelCC(x, y, z + 1))
-					build_face(origin + vec3_forward, vec3_up, vec3_right, false, false, vertices_ptr, normals_ptr, uvs_ptr, indices_ptr);
+				if (frontFace)
+				{
+					ao00 = calcAO(block_mfl, block_bf, block_bfl);
+					ao01 = calcAO(block_mfl, block_tf, block_tfl);
+					ao11 = calcAO(block_mfr, block_tf, block_tfr);
+					ao10 = calcAO(block_mfr, block_bf, block_bfr);
+
+					auto flippedQuad = ao00 + ao11 > ao01 + ao10;
+
+					build_face(origin + vec3_forward, vec3_up, vec3_right, false, flippedQuad, vertices_ptr, normals_ptr, uvs_ptr, indices_ptr);
+
+					colors_ptr->push_back({ ao00 * ambientStr, 0.0f, 0.0f, 0.0f });
+					colors_ptr->push_back({ ao01 * ambientStr, 0.0f, 0.0f, 0.0f });
+					colors_ptr->push_back({ ao11 * ambientStr, 0.0f, 0.0f, 0.0f });
+					colors_ptr->push_back({ ao10 * ambientStr, 0.0f, 0.0f, 0.0f });
+				}
 
 				// back face
-				if (!getVoxelCC(x, y, z - 1))
-					build_face(origin, vec3_up, vec3_right, true, false, vertices_ptr, normals_ptr, uvs_ptr, indices_ptr);
+				if (backFace)
+				{
+					ao00 = calcAO(block_bb, block_mbl, block_bbl);
+					ao01 = calcAO(block_mbl, block_tb, block_tbl);
+					ao11 = calcAO(block_mbr, block_tb, block_tbr);
+					ao10 = calcAO(block_mbr, block_bb, block_bbr);
+
+					auto flippedQuad = ao00 + ao11 > ao01 + ao10;
+
+					build_face(origin, vec3_up, vec3_right, true, flippedQuad, vertices_ptr, normals_ptr, uvs_ptr, indices_ptr);
+
+					colors_ptr->push_back({ ao00 * ambientStr, 0.0f, 0.0f, 0.0f });
+					colors_ptr->push_back({ ao01 * ambientStr, 0.0f, 0.0f, 0.0f });
+					colors_ptr->push_back({ ao11 * ambientStr, 0.0f, 0.0f, 0.0f });
+					colors_ptr->push_back({ ao10 * ambientStr, 0.0f, 0.0f, 0.0f });
+				}
 			}
 		}
 	}
@@ -173,7 +310,8 @@ void VoxelChunk::generateMesh(
 	m_mesh = Mesh::createMesh();
 	m_mesh->setVertices(vertices_ptr->data(), uint(vertices_ptr->size()));
 	m_mesh->setUVs(uvs_ptr->data());
-	m_mesh->setNormals(normals_ptr ->data());
+	m_mesh->setNormals(normals_ptr->data());
+	m_mesh->setColors(colors_ptr->data());
 	m_mesh->setIndices(indices_ptr->data(), uint(indices_ptr->size()));
 
 	m_mesh->applyChanges();
