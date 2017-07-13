1fe4e6661641df867962d03e24c6381ff1f16d68
Damian Korczowski <erdroy2@gmail.com>
2017-07-13 21:37:33 +0200

    Updated libs build script and updated gitignore

--- generate_libs.ps1
+++ generate_libs.ps1
@@ -1,5 +1,5 @@
 del /s /q libs_temp
 
-robocopy libs libs_temp *.h *.hpp *.lib *.dll *.def *.pdb /s
+robocopy libs libs_temp *.h *.hpp *.lib *.dll *.def *.pdb .git /s
 
 Compress-Archive libs_temp libs_prebuilt.zip
