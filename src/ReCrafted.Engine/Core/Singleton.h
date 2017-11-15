abd6ba9695e3b9bdc007ff30cef967829969fb6a
Damian Korczowski <erdroy2@gmail.com>
2017-11-15 15:27:43 +0100

    Added Singleton<T> class.

---
+++ src/ReCrafted.Engine/Core/Singleton.h
@@ -0,0 +1,39 @@
+// ReCrafted © 2016-2017 Always Too Late
+
+#pragma once
+
+#ifndef SINGLETON_H
+#define SINGLETON_H
+
+#include "ReCrafted.h"
+
+template<class T>
+class Singleton
+{
+	static T* m_instance;
+
+protected:
+	Singleton() {}
+	~Singleton() {}
+
+private:
+	virtual void onDispose() = 0;
+
+public:
+	void dispose() const
+	{
+		SafeDelete(m_instance);
+	}
+
+public:
+	static T* getInstance()
+	{
+		if (m_instance)
+			return m_instance;
+
+		m_instance = new T;
+		return m_instance;
+	}
+};
+
+#endif // SINGLETON_H
