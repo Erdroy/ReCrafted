6b638b712e08bbc8e2c0681d6aec1d077a4ac8e6
Damian Korczowski <erdroy2@gmail.com>
2017-12-07 17:31:31 +0100

    [Engine][Profiler] Added self profiling for Profiler.

--- src/ReCrafted.Engine/Common/Profiler/Profiler.cpp
+++ src/ReCrafted.Engine/Common/Profiler/Profiler.cpp
@@ -50,63 +50,67 @@ void Profiler::drawDebugScreen()
 	if (!m_drawDebugScreen)
 		return;
 
-	var depth = UI::getDepth();
-	UI::setDepth(9999.0f);
-
-	var yOffset = 0.0f;
-
-	float gcUsedSize;
-	var unitA = ByteFormat::Format(mono_gc_get_used_size(), &gcUsedSize);
-
-	float gcHeapSize;
-	var unitB = ByteFormat::Format(mono_gc_get_heap_size(), &gcHeapSize);
-
-	UI::setColor(Color(0xFF0A00FF));
-	UI::drawText(m_debugFont, TEXT_CONST("Profiler [press F9 to hide]"), Vector2(0.0f, 0.0f));
-	yOffset += static_cast<float>(m_debugFont->getSize()) * 2.0f;
-
-	UI::setColor(Color(0xFFFFFFFF));
-	UI::drawText(m_debugFont, TEXT_CONST("[GC Memory]"), Vector2(0.0f, yOffset));
-	yOffset += static_cast<float>(m_debugFont->getSize()) * 2.0f;
-
-	UI::drawText(m_debugFont, 
-		Text::format(TEXT("Used size: {0} {1}\nHeap size: {2} {3}"), 
-		gcUsedSize, ByteFormat::ToString(unitA), gcHeapSize, ByteFormat::ToString(unitB)),
-		Vector2(0.0f, yOffset)
-	);
-	yOffset += static_cast<float>(m_debugFont->getSize()) * 3.0f;
-
-	UI::drawText(m_debugFont, TEXT_CONST("[Profiles]"), Vector2(0.0f, yOffset));
-	yOffset += static_cast<float>(m_debugFont->getSize()) * 2.0f;
-
-	// TODO: update buffer and calculate avg
-	// delete all entries that are older than one second
-	// add all the current
-	// calulate avg
-
-	for (var i = static_cast<int>(m_lastFrame.count()) - 1; i >= 0; i --)
+	beginProfile(TEXT_CONST("Profiler Draw (self profile)"), 0.5f, 1.0f);
 	{
+		var depth = UI::getDepth();
+		UI::setDepth(9999.0f);
+
+		var yOffset = 0.0f;
+
+		float gcUsedSize;
+		var unitA = ByteFormat::Format(mono_gc_get_used_size(), &gcUsedSize);
+
+		float gcHeapSize;
+		var unitB = ByteFormat::Format(mono_gc_get_heap_size(), &gcHeapSize);
+
+		UI::setColor(Color(0xFF0A00FF));
+		UI::drawText(m_debugFont, TEXT_CONST("Profiler [press F9 to hide]"), Vector2(0.0f, 0.0f));
+		yOffset += static_cast<float>(m_debugFont->getSize()) * 2.0f;
+
 		UI::setColor(Color(0xFFFFFFFF));
+		UI::drawText(m_debugFont, TEXT_CONST("[GC Memory]"), Vector2(0.0f, yOffset));
+		yOffset += static_cast<float>(m_debugFont->getSize()) * 2.0f;
 
-		var entry = m_lastFrame.at(i);
-		var depthOffset = entry.depth * 15.0f;
+		UI::drawText(m_debugFont,
+			Text::format(TEXT("Used size: {0} {1}\nHeap size: {2} {3}"),
+				gcUsedSize, ByteFormat::ToString(unitA), gcHeapSize, ByteFormat::ToString(unitB)),
+			Vector2(0.0f, yOffset)
+		);
+		yOffset += static_cast<float>(m_debugFont->getSize()) * 3.0f;
 
-		if (entry.time > entry.timeMax && entry.timeMax >= 0.0f)
-			UI::setColor(Color(0xFF0A00FF));
-		else if (entry.time > entry.timeMed && entry.timeMed >= 0.0f)
-			UI::setColor(Color(0xAA0A00FF));
-		else
+		UI::drawText(m_debugFont, TEXT_CONST("[Profiles]"), Vector2(0.0f, yOffset));
+		yOffset += static_cast<float>(m_debugFont->getSize()) * 2.0f;
+
+		// TODO: update buffer and calculate avg
+		// delete all entries that are older than one second
+		// add all the current
+		// calulate avg
+
+		for (var i = static_cast<int>(m_lastFrame.count()) - 1; i >= 0; i--)
+		{
 			UI::setColor(Color(0xFFFFFFFF));
 
-		var infoText = Text::format(TEXT("{0:2f} ms"), entry.time);
+			var entry = m_lastFrame.at(i);
+			var depthOffset = entry.depth * 15.0f;
 
-		UI::drawText(m_debugFont, infoText, Vector2(10.0f, yOffset));
-		UI::drawText(m_debugFont, entry.name, Vector2(150.0f + depthOffset, yOffset));
+			if (entry.time > entry.timeMax && entry.timeMax >= 0.0f)
+				UI::setColor(Color(0xFF0A00FF));
+			else if (entry.time > entry.timeMed && entry.timeMed >= 0.0f)
+				UI::setColor(Color(0xAA0A00FF));
+			else
+				UI::setColor(Color(0xFFFFFFFF));
 
-		yOffset += m_debugFont->getSize();
+			var infoText = Text::format(TEXT("{0:2f} ms"), entry.time);
+
+			UI::drawText(m_debugFont, infoText, Vector2(10.0f, yOffset));
+			UI::drawText(m_debugFont, entry.name, Vector2(150.0f + depthOffset, yOffset));
+
+			yOffset += m_debugFont->getSize();
+		}
+
+		UI::setDepth(depth);
 	}
-
-	UI::setDepth(depth);
+	endProfile();
 }
 
 void Profiler::endFrame()
