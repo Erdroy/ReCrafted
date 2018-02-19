5a16f1f4859b801224c68c074bfecba78e769c0a
Damian Korczowski <erdroy2@gmail.com>
2018-02-19 21:15:13 +0100

    [Core] Added Prerequisites header

--- src/ReCrafted.Engine/ReCrafted.h
+++ src/ReCrafted.Engine/ReCrafted.h
@@ -7,26 +7,7 @@
 #ifndef RECRAFTED_H
 #define RECRAFTED_H
 
-#ifdef _WIN32
-#include <Windows.h>
-#endif
-
-#include <ctime>
-#include <vector>
-
-#include "bgfxPrerequisites.h"
-
-#include "Core/IDisposable.h"
-
-#include "Core/Defines.h"
-#include "Core/Types.h"
-#include "Core/Logger.h"
-#include "Common/ReCraftedAPI.h"
-
-#ifdef var
-#	undef var
-#endif
-#define var auto
-#define cvar const auto
+// includes prereqs
+#include "ReCraftedPrerequisites.h"
 
 #endif // RECRAFTED_H
---
+++ src/ReCrafted.Engine/ReCraftedPrerequisites.h
@@ -0,0 +1,53 @@
+// ReCrafted (c) 2016-2018 Always Too Late
+
+#pragma once
+
+#ifndef RECRAFTEDPREREQUISITES_H
+#define RECRAFTEDPREREQUISITES_H
+
+#ifdef _WIN32
+// TODO: remove this...
+#include <Windows.h>
+#endif
+
+#include <ctime>
+#include <vector>
+
+#include "bgfxPrerequisites.h"
+
+#include "Core/IDisposable.h"
+#include "Core/Defines.h"
+#include "Core/Types.h"
+#include "Core/Logger.h"
+#include "Common/ReCraftedAPI.h"
+
+#ifdef var
+#	undef var
+#endif
+#define var auto
+#define cvar const auto
+
+class IDisposable;
+
+class ApplicationBase;
+class ApplicationWindow;
+
+class UpdateLoop;
+
+class EngineMain;
+class EngineComponent;
+class EngineComponentManager;
+
+class Logger;
+
+class ScriptingEngine;
+class Bindings;
+class Domain;
+class Assembly;
+class Class;
+class Field;
+class Method;
+class Object;
+class Script;
+
+#endif // RECRAFTEDPREREQUISITES_H
