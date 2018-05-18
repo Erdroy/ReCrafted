2308b60303e6757ef2a46abedaaee4b8882bd0d8
Damian Korczowski <erdroy2@gmail.com>
2018-05-18 22:17:15 +0200

    Added missing new/[] and delete/[] implementations

--- src/ReCrafted.Engine/Core/Memory.h
+++ src/ReCrafted.Engine/Core/Memory.h
@@ -12,25 +12,52 @@
 #if COMPILE_WITH_RPMALLOC
 #pragma warning( push )
 #pragma warning( disable : 4595)
-inline void* operator new(const std::size_t size) noexcept
-{
+inline void* operator new(size_t size) {
     return rpmalloc(size);
 }
 
-inline void* operator new[](const std::size_t size) noexcept
-{
+inline void operator delete(void* ptr) noexcept {
+    if (rpmalloc_is_thread_initialized())
+        rpfree(ptr);
+}
+
+inline void* operator new[](size_t size) {
     return rpmalloc(size);
 }
 
-inline void operator delete(void* ptr) noexcept
-{
-    rpfree(ptr);
+inline void operator delete[](void* ptr) noexcept {
+    if (rpmalloc_is_thread_initialized())
+        rpfree(ptr);
 }
 
-inline void operator delete[](void* ptr) noexcept
-{
-    rpfree(ptr);
+inline void* operator new(size_t size, const std::nothrow_t&) noexcept {
+    return rpmalloc(size);
 }
+
+inline void* operator new[](size_t size, const std::nothrow_t&) noexcept {
+    return rpmalloc(size);
+}
+
+inline void operator delete(void* ptr, const std::nothrow_t&) noexcept {
+    if (rpmalloc_is_thread_initialized())
+        rpfree(ptr);
+}
+
+inline void operator delete[](void* ptr, const std::nothrow_t&) noexcept {
+    if (rpmalloc_is_thread_initialized())
+        rpfree(ptr);
+}
+
+inline void operator delete(void* ptr, size_t) noexcept {
+    if (rpmalloc_is_thread_initialized())
+        rpfree(ptr);
+}
+
+inline void operator delete[](void* ptr, size_t) noexcept {
+    if (rpmalloc_is_thread_initialized())
+        rpfree(ptr);
+}
+
 #pragma warning( pop )
 #endif
 
