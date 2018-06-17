495c70c713f316fa0aa942a075f90a4d67d3343a
Damian Korczowski <erdroy2@gmail.com>
2018-06-17 14:37:26 +0200

    Added proper batching for debug draw.

--- src/ReCrafted.Engine/Graphics/DebugDraw.cpp
+++ src/ReCrafted.Engine/Graphics/DebugDraw.cpp
@@ -6,7 +6,6 @@ SINGLETON_IMPL(DebugDraw)
 
 void DebugDraw::OnInit()
 {
-    m_lines.Reserve(4096); // 2048 lines at the start
 }
 
 void DebugDraw::OnLoad()
@@ -17,20 +16,83 @@ void DebugDraw::Update()
 {
 }
 
+void DebugDraw::RenderLines(const Batch& batch)
+{
+
+}
+
+void DebugDraw::RenderTriangles(const Batch& batch)
+{
+
+}
+
 void DebugDraw::Render()
 {
-    
+    for (rvar batch : m_batches)
+    {
+        RenderLines(batch);
+        RenderTriangles(batch);
 
-    // Clear lines at the end
-    m_lines.Clear();
+        batch.Clear();
+    }
+
+    // We don't want to remove batches, but only reset to the first one
+    m_currentBatch.reset(&m_batches.First());
 }
 
 void DebugDraw::OnDispose()
 {
 }
 
+void DebugDraw::DrawLine(const Vector3& start, const Vector3& end)
+{
+    // Add new line to the current batch
+    rvar batch = GetBatch();
+    rvar lineList = batch->GetLineList();
+
+    lineList.Add(Point{start, m_currentColor});
+    lineList.Add(Point{end, m_currentColor});
+}
+
+RefPtr<DebugDraw::Batch>& DebugDraw::GetBatch()
+{
+    if (m_currentBatch->IsFull())
+    {
+        ASSERT(m_batches.Count() < m_maxBatches);
+
+        // Check if there is next batch available
+        // - if not: create new batch
+        // Use new batch
+        cvar idx = m_batches.IndexOf(*m_currentBatch.get());
+
+        if (idx < static_cast<int>(m_batches.Size()) - 1)
+        {
+            // Select next batch
+            m_currentBatch.reset(&m_batches[idx + 1]);
+        }
+        else
+        {
+            // Create new batch
+            m_batches.Add({});
+
+            // Set the new batch as first
+            m_currentBatch.reset(&m_batches.Last());
+        }
+    }
+
+    return m_currentBatch;
+}
+
+void DebugDraw::SetColor(const Color& color)
+{
+    ASSERT(IS_MAIN_THREAD());
+
+    m_instance->m_currentColor = color;
+}
+
 void DebugDraw::DrawBox(const Vector3& position, const Vector3& size)
 {
-    m_instance->m_lines.Add(position);
-    m_instance->m_lines.Add(position + Vector3::Up());
+    ASSERT(IS_MAIN_THREAD());
+
+    m_instance->DrawLine(position, position + Vector3::Up());
 }
--- src/ReCrafted.Engine/Graphics/DebugDraw.h
+++ src/ReCrafted.Engine/Graphics/DebugDraw.h
@@ -8,24 +8,141 @@
 // includes
 #include "ReCrafted.h"
 #include "Core/EngineComponent.h"
-#include "Core/Math/Vector3.h"
 #include "Core/Containers/Array.h"
+#include "Core/Math/Math.h"
+#include "Core/Math/Color.h"
 
+/**
+ * \brief The DebugDraw class. Provides drawing functions for debugging.
+ */
 class DebugDraw : public EngineComponent<DebugDraw>
 {
     friend class Graphics;
 
 private:
-    Array<Vector3> m_lines = {};
+    const uint m_maxBatches = 128;
+
+private:
+    struct ALIGN(4) Vertex
+    {
+        Vector3 position = {};
+        Color color = {};
+
+        Vertex(const Vector3& _position, const Color& _color) : position(_position), color(_color) { }
+    };
+
+    struct ALIGN(4) Point
+    {
+        Vector3 position = {};
+        Color color = {};
+
+        Point(const Vector3& _position, const Color& _color) : position(_position), color(_color) { }
+    };
+
+    class Batch
+    {
+    private:
+        const uint m_maxVerticesPerBatch = 65536;
+        const uint m_maxPointsPerBatch = m_maxVerticesPerBatch;
+
+    private:
+        Array<Point> m_lines = {};
+        Array<Vertex> m_vertices = {};
+
+    public:
+        /**
+         * \brief Default Batch constructor.
+         */
+        Batch()
+        {
+            // Reserve memory for vertices and lines.
+            m_vertices.Reserve(m_maxVerticesPerBatch);
+            m_lines.Reserve(m_maxPointsPerBatch);
+        }
+
+    public:
+        /**
+         * \brief Clears the batch.
+         */
+        void Clear()
+        {
+            // Clear vertices and lines
+            m_lines.Clear();
+            m_vertices.Clear();
+        }
+
+        /**
+         * \brief Returns true when this batch has some proper data to draw a shape.
+         */
+        bool HasData() const
+        {
+            return m_lines.Count() >= 2 || m_vertices.Count() >= 3;
+        }
+
+        /**
+         * \brief Returns false when there is no more space to push any new data.
+         */
+        bool IsFull() const
+        {
+            return m_lines.Count() >= m_maxPointsPerBatch - 2
+                || m_vertices.Count() >= m_maxVerticesPerBatch - 3;
+        }
+
+        /**
+         * \brief Gets line list from this batch.
+         */
+        Array<Point>& GetLineList()
+        {
+            return m_lines;
+        }
+
+        /**
+        * \brief Gets vertex list from this batch.
+        */
+        Array<Vertex>& GetVertexList()
+        {
+            return m_vertices;
+        }
+
+    public:
+        inline bool operator==(const Batch& rhs)
+        {
+            return this == &rhs;
+        }
+    };
+
+private:
+    Array<Batch> m_batches;
+    RefPtr<Batch> m_currentBatch;
+    Color m_currentColor;
+
+public:
+    DebugDraw()
+    {
+        // Add one batch
+        m_batches = {};
+        m_batches.Add({});
+        m_currentBatch.reset(&m_batches[0]);
+        m_currentColor = Color(0xFF0000FF);
+    }
 
 private:
     void OnInit() override;
     void OnLoad() override;
     void Update() override;
+
+    void RenderLines(const Batch& batch);
+    void RenderTriangles(const Batch& batch);
+
     void Render();
     void OnDispose() override;
 
+    void DrawLine(const Vector3& start, const Vector3& end);
+
+    RefPtr<Batch>& GetBatch();
+
 public:
+    static void SetColor(const Color& color);
     static void DrawBox(const Vector3& position, const Vector3& size);
 };
 
