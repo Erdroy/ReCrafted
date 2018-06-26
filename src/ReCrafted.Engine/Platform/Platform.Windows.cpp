9de8a049c42c6f4cec1d0ecb9daa9544c59202f1
Damian Korczowski <erdroy2@gmail.com>
2018-06-26 15:05:12 +0200

    Throw exception using formated text instead of expression

--- src/ReCrafted.Engine/Platform/Platform.Windows.cpp
+++ src/ReCrafted.Engine/Platform/Platform.Windows.cpp
@@ -344,7 +344,7 @@ void Platform::ReportAssert(Text expression, Text fileName, unsigned int line, T
 #endif
 
 #if ASSERT_FAIL_EXCEPTION
-    throw std::exception(expression.StdStr().c_str());
+    throw std::exception(formated.c_str());
 #else
     // and exit...
     exit(-1);
