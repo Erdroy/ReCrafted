3c90cb6a67cb9e19aab67575b5d3c00e41f6d140
Damian Korczowski <erdroy2@gmail.com>
2018-08-08 11:22:35 +0200

    Terrain shader update

--- src/ReCrafted.Engine/Shaders/Shaders/TerrainShader.hlsl
+++ src/ReCrafted.Engine/Shaders/Shaders/TerrainShader.hlsl
@@ -6,6 +6,11 @@
 #define USE_LOGZBUFFER
 #include "../ShaderAPI.hlsli"
 
+Texture2D m_texture0 : register(t0);
+Texture2D m_texture1 : register(t1);
+
+SamplerState<LinearWrap> Sampler : register(s0);
+
 struct TerrainVSInput
 {
     float3 Position : POSITION;
@@ -18,12 +23,14 @@ struct TerrainVSOutput
 {
     float4 Position : SV_POSITION;
     float3 LocalPosition : WS_POSITION;
-    float3 Normal   : NORMAL;
+    float3 Normal   : NORMAL0;
+    float3 FlatNormal : NORMAL1;
     float4 Color    : COLOR0;
     uint2 Materials : TEXCOORD0;
+    float Blend : TEXCOORD1;
 
 #ifdef USE_LOGZBUFFER
-    float LogZ : TEXCOORD1;
+    float LogZ : TEXCOORD2;
 #endif
 };
 
@@ -40,8 +47,10 @@ void TerrainVSMain(in TerrainVSInput i, out TerrainVSOutput o)
     o.Position = position;
     o.LocalPosition = i.Position;
     o.Normal = i.Normal;
+    o.FlatNormal = (float3)0;
     o.Color = i.Color;
     o.Materials = i.Materials;
+    o.Blend = ((i.Materials[0] >> 8) & 0xFF) / 255.f;
 
     // TODO: Vertex lighting
 
@@ -60,15 +69,17 @@ void TerrainGSMain(triangle TerrainVSOutput input[3], inout TriangleStream<Terra
     TerrainPSInput output;
     
     // Calculate output normal for all vertices
-    output.Normal = normalize(input[0].Normal + input[1].Normal + input[2].Normal);
+    output.FlatNormal = normalize(input[0].Normal + input[1].Normal + input[2].Normal);
 
     // TODO: Calculate flat lighting
 
     // Add 1st triangle
     output.Position = input[0].Position;
     output.LocalPosition = input[0].LocalPosition;
+    output.Normal = input[0].Normal;
     output.Color = input[0].Color;
     output.Materials = input[0].Materials;
+    output.Blend = input[0].Blend;
 #ifdef USE_LOGZBUFFER
     output.LogZ = input[0].LogZ;
 #endif
@@ -77,8 +88,10 @@ void TerrainGSMain(triangle TerrainVSOutput input[3], inout TriangleStream<Terra
     // Add 2nd triangle
     output.Position = input[1].Position;
     output.LocalPosition = input[1].LocalPosition;
+    output.Normal = input[1].Normal;
     output.Color = input[1].Color;
     output.Materials = input[1].Materials;
+    output.Blend = input[0].Blend;
 #ifdef USE_LOGZBUFFER
     output.LogZ = input[1].LogZ;
 #endif
@@ -87,8 +100,10 @@ void TerrainGSMain(triangle TerrainVSOutput input[3], inout TriangleStream<Terra
     // Add 3rd triangle
     output.Position = input[2].Position;
     output.LocalPosition = input[2].LocalPosition;
+    output.Normal = input[2].Normal;
     output.Color = input[2].Color;
     output.Materials = input[2].Materials;
+    output.Blend = input[0].Blend;
 #ifdef USE_LOGZBUFFER
     output.LogZ = input[2].LogZ;
 #endif
@@ -97,14 +112,33 @@ void TerrainGSMain(triangle TerrainVSOutput input[3], inout TriangleStream<Terra
     OutputStream.RestartStrip();
 }
 
+float3 TmpGetMatColor(in uint matId) 
+{
+    switch (matId)
+    {
+    case 1:
+        return float3(0.35f, 0.35f, 0.35f);
+    case 2:
+        return float3(80 / 255.0f, 145 / 255.0f, 30 / 255.0f);
+
+    case 255:
+        return float3(1.0f, 0.0f, 0.0f);
+
+    default:
+        return float3(0.1f, 0.0f, 0.1f);
+    }
+}
+
 /// <summary>
 /// Pixel Shader Function
 /// TerrainPSMain
 /// </summary>
 void TerrainPSMain(in TerrainPSInput i, out GBufferOutput o)
 {
+    const float3 normal = normalize(i.Normal);
+
     // Blending factor of triplanar mapping
-    float3 blend = pow(i.Normal.xyz, 4);
+    float3 blend = pow(i.Normal.xyz, 4); // TODO: Improve blend function
     blend /= dot(blend, float3(1, 1, 1));
 
     // Triplanar mapping
@@ -116,16 +150,31 @@ void TerrainPSMain(in TerrainPSInput i, out GBufferOutput o)
     float3 colorB = float3(0.0f, 1.0f, 0.0f);
     float3 colorC = float3(0.0f, 0.0f, 1.0f);
 
-    //o.Color = float4((colorA * blend.x) + (colorB * blend.y) + (colorC * blend.z), 1.0f);
+    // Txz, Uxz
+    float2 material1 = float2(i.Materials[0] >> 24, (i.Materials[0] >> 16) & 0xFF);
 
-    o.Color = i.Color;
+    // Tpy, Tny, Upy, Uny
+    float4 material2 = float4(i.Materials[1] >> 24, (i.Materials[1] >> 16) & 0xFF, (i.Materials[1] >> 8) & 0xFF, (i.Materials[1]) & 0xFF);
 
-    // TODO: Consider whenever we need to use texture sampling, raw vertex color or textureColor * vertexColor
-    //o.Color = float4(tx, 0.0f, 1.0f);
-    o.Normal = EncodeNormal(float4(i.Normal, 1.0f));
+    float3 flip = float3(normal.x < 0.0, normal.y < 0.0, normal.z >= 0.0);
+    float2 zindex = lerp(material2.xz, material2.yw, flip.y);
+
+    float3 cx0 = TmpGetMatColor(material1.x);
+    float3 cz0 = TmpGetMatColor(material1.x);
+    float3 cy0 = TmpGetMatColor(zindex.x);
+
+    float3 cx1 = TmpGetMatColor(material1.y);
+    float3 cz1 = TmpGetMatColor(material1.y);
+    float3 cy1 = TmpGetMatColor(zindex.y);
+
+    float3 albedo = lerp((blend.x * cx0 + blend.y * cy0 + blend.z * cz0), (blend.x * cx1 + blend.y * cy1 + blend.z * cz1), i.Blend);
+
+    o.Color = float4(albedo, 1.0f);
+
+    o.Normal = EncodeNormal(float4(i.FlatNormal, 1.0f));
 
     // Set Light as simple ambient light
-    o.Light = float4(i.Color.rgb * AmbientLightColor, 0.0f);
+    o.Light = float4(o.Color.rgb * AmbientLightColor, 0.0f);
 
 #ifdef USE_LOGZBUFFER
     // TODO: Make option to use inv z (1.0f - Z/W) instead of log z,
