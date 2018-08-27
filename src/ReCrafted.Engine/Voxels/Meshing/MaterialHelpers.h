cf60514c4d92c8bb65fc199d85ae5cb3eada1a1a
Damian Korczowski <erdroy2@gmail.com>
2018-08-27 21:11:54 +0200

    Added SetupMaterial function

--- src/ReCrafted.Engine/Voxels/Meshing/MaterialHelpers.h
+++ src/ReCrafted.Engine/Voxels/Meshing/MaterialHelpers.h
@@ -9,21 +9,17 @@
 #include "ReCrafted.h"
 
 /**
-* \brief Calculates materials from two given voxels.
-* \param materialMap The material map pointer.
-* \param lod The level of detail.
-* \param voxelA The first voxel.
-* \param voxelB The second voxel.
-* \return The selected material.
+* \brief Setups material from given voxel.
+* \param materialMap The material map instance.
+* \param voxel The voxel that contain material information.
+* \return The resulting vertex material.
 */
-inline VertexMaterial_t CalculateMaterials(IVoxelMaterialMap* materialMap, const int lod, const Voxel& voxelA, const Voxel& voxelB)
+inline VertexMaterial_t SetupMaterial(IVoxelMaterialMap* materialMap, const Voxel& voxel)
 {
+    cvar matSet = materialMap->GetMaterial(voxel.material);
+
     VertexMaterial_t material = {};
-
-    cvar minimalVoxel = GetMinimalVoxel(voxelA, voxelB);
-    cvar matSet = materialMap->GetMaterial(minimalVoxel.material);
-
-    material.Blend = minimalVoxel.blend;
+    material.Blend = voxel.blend;
 
     material.Tny = matSet->MaterialIdsA[0];
     material.Tpy = matSet->MaterialIdsA[1];
@@ -34,6 +30,20 @@ inline VertexMaterial_t CalculateMaterials(IVoxelMaterialMap* materialMap, const
     material.Uxz = matSet->MaterialIdsB[2];
 
     return material;
+}
+
+/**
+* \brief Calculates materials from two given voxels.
+* \param materialMap The material map pointer.
+* \param lod The level of detail.
+* \param voxelA The first voxel.
+* \param voxelB The second voxel.
+* \return The resulting vertex material.
+*/
+inline VertexMaterial_t CalculateMaterials(IVoxelMaterialMap* materialMap, const int lod, const Voxel& voxelA, const Voxel& voxelB)
+{
+    cvar minimalVoxel = GetMinimalVoxel(voxelA, voxelB);
+    return SetupMaterial(materialMap, minimalVoxel);
 }
 
 #endif // MATERIALHELPERS_H
