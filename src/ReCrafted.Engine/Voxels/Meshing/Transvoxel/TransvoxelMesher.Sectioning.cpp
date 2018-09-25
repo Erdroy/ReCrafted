b3660dd6a7e0e464882bf1f7a202c15cfa3bfabc
Damian Korczowski <erdroy2@gmail.com>
2018-09-25 15:29:42 +0200

    Transvoxel mesher source code cleanup

---
+++ src/ReCrafted.Engine/Voxels/Meshing/Transvoxel/TransvoxelMesher.Sectioning.cpp
@@ -0,0 +1,86 @@
+// ReCrafted (c) 2016-2018 Always Too Late
+
+#include "TransvoxelMesher.h"
+
+TransvoxelMesher::MeshSection* TransvoxelMesher::FindSection(const VoxelMaterial_t material)
+{
+    for (rvar section : m_meshSections)
+    {
+        if (section.HasMaterial(material))
+            return &section;
+    }
+
+    return nullptr;
+}
+
+TransvoxelMesher::MeshSection* TransvoxelMesher::FindSection(
+    const VoxelMaterial_t materialA,
+    const VoxelMaterial_t materialB,
+    const VoxelMaterial_t materialC)
+{
+    for (rvar section : m_meshSections)
+    {
+        if (section.HasMaterials(materialA, materialB, materialC))
+            return &section;
+    }
+
+    return nullptr;
+}
+
+TransvoxelMesher::MeshSection* TransvoxelMesher::PushSection(
+    const VoxelMaterial_t materialA,
+    const VoxelMaterial_t materialB,
+    const VoxelMaterial_t materialC)
+{
+    for (var i = m_currentSection; i < MaxSections; i++)
+    {
+        rvar section = m_meshSections[i];
+
+        if (section.CanFit(materialA, materialB, materialC))
+        {
+            section.AddMaterial(materialA);
+            section.AddMaterial(materialB);
+            section.AddMaterial(materialC);
+            return &section;
+        }
+    }
+
+    var section = CreateSection();
+    section->AddMaterial(materialA);
+    section->AddMaterial(materialB);
+    section->AddMaterial(materialC);
+
+    return section;
+}
+
+TransvoxelMesher::MeshSection* TransvoxelMesher::FindOrPushSection(
+    const VoxelMaterial_t materialA,
+    const VoxelMaterial_t materialB,
+    const VoxelMaterial_t materialC)
+{
+    var meshSection = FindSection(materialA, materialB, materialC);
+
+    if (!meshSection)
+        meshSection = PushSection(materialA, materialB, materialC);
+
+    return meshSection;
+}
+
+TransvoxelMesher::MeshSection* TransvoxelMesher::CreateSection()
+{
+    m_currentSection++;
+    ASSERT(m_currentSection < MaxSections); // TODO: Handle error
+    return &GetSection();
+}
+
+void TransvoxelMesher::AddTriangle(const VertexInfo& vertexInfoA, const VertexInfo& vertexInfoB, const VertexInfo& vertexInfoC, const bool normalCorrection)
+{
+    var meshSection = FindOrPushSection(vertexInfoA.voxelMaterial, vertexInfoB.voxelMaterial, vertexInfoC.voxelMaterial);
+
+    ASSERT(meshSection != nullptr);
+
+    // Add data to the section
+    meshSection->AddVertex(vertexInfoA, normalCorrection);
+    meshSection->AddVertex(vertexInfoB, normalCorrection);
+    meshSection->AddVertex(vertexInfoC, normalCorrection);
+}
