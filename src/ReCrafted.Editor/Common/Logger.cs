6be11aa5eea782b5f767fc5207b5edb35a3cfdb6
Damian Korczowski <erdroy2@gmail.com>
2018-09-30 22:38:14 +0200

    Added simple logger for editor

---
+++ src/ReCrafted.Editor/Common/Logger.cs
@@ -0,0 +1,70 @@
+﻿// ReCrafted Editor (c) 2016-2018 Always Too Late
+
+using System;
+using System.IO;
+
+namespace ReCrafted.Editor.Common
+{
+    public class Logger
+    {
+        public enum LogType
+        {
+            Info,
+            Warning,
+            Error,
+            Exception
+        }
+
+        private static Logger _instance;
+        private readonly FileStream _fileStream;
+        private readonly StreamWriter _stream;
+
+        public Logger()
+        {
+            _fileStream = new FileStream("log.txt", FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+            _stream = new StreamWriter(_fileStream);
+        }
+
+        ~Logger()
+        {
+            _fileStream.Dispose();
+        }
+
+        protected virtual void OnLogWritten(string obj, LogType logType)
+        {
+            LogWritten?.Invoke(obj, logType);
+        }
+
+        public static void Log(string message, LogType logType)
+        {
+            var logMessage = $"{DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss")} [{logType.ToString()}]: {message}\n";
+            Instance._stream.Write(logMessage);
+            Instance.OnLogWritten(logMessage, logType);
+        }
+
+        public static void Log(string message)
+        {
+            Log(message, LogType.Info);
+        }
+
+        public static void LogWarning(string message)
+        {
+            Log(message, LogType.Warning);
+        }
+
+        public static void LogError(string message)
+        {
+            Log(message, LogType.Error);
+        }
+
+        public static void LogException(Exception exception)
+        {
+            // TODO: Format exception
+            Log(exception.Message, LogType.Exception);
+        }
+
+        public event Action<string, LogType> LogWritten;
+
+        public static Logger Instance => _instance ?? (_instance = new Logger());
+    }
+}
