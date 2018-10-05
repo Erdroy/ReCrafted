5f76e94dbf3c00ed3adc3b0404752f4ffd40783a
Damian Korczowski <erdroy2@gmail.com>
2018-10-05 16:59:31 +0200

    Implemented new Texture asset.

--- src/ReCrafted.Engine/Graphics/Texture.cpp
+++ src/ReCrafted.Engine/Graphics/Texture.cpp
@@ -4,19 +4,28 @@
 
 void Texture::OnInitialize()
 {
+    Apply();
 }
 
 void Texture::OnLoadBegin(const std::string& fileName)
 {
-    // Load first mip
-
+    
 }
 
 void Texture::OnDeserializeBinary(uint16_t version, BinaryStream& stream)
 {
+    cvar totalSize = stream.ReadUInt32();
     m_format = stream.ReadUInt16();
     m_mips = stream.ReadByte();
 
+    // TODO: Lazy texture loading
+
+    // Allocate memory for this texture.
+    cvar memory = reinterpret_cast<char*>(Renderer::Allocate(totalSize, 0));
+
+    ASSERT(memory);
+
+    var memoryOffset = 0;
     for(var mipId = 0u; mipId < m_mips; mipId ++)
     {
         cvar width = stream.ReadUInt16();
@@ -24,14 +33,157 @@ void Texture::OnDeserializeBinary(uint16_t version, BinaryStream& stream)
         cvar pitch = stream.ReadUInt16();
         cvar size = stream.ReadInt32();
         
+        ASSERT(width > 0);
+        ASSERT(height > 0);
+        ASSERT(pitch > 0);
+        ASSERT(size > 0);
 
+        if(mipId == 0u)
+        {
+            m_width = width;
+            m_height = height;
+            m_pitch = pitch;
+        }
+
+        // Read mip data
+        stream.ReadBytes(memory + memoryOffset, size);
+        memoryOffset += size;
     }
+
+    // Set memory pointer
+    m_textureMemory = reinterpret_cast<Renderer::RendererMemory>(memory);
+    m_textureMemorySize = totalSize;
 }
 
 void Texture::OnUnload()
 {
+    if(m_textureMemory)
+    {
+        Renderer::Free(m_textureMemory);
+    }
+
+    if (RENDERER_CHECK_HANDLE(m_textureHandle))
+    {
+        Renderer::DestroyTexture2D(m_textureHandle);
+    }
 }
 
-void Texture::LoadFromMemory(uint8_t* data, uint16_t width, uint16_t height, uint8_t mips, uint8_t bitsPerPixel)
+void Texture::Initialize(const uint16_t width, const uint16_t height)
 {
+    cvar textureFormat = Renderer::TextureFormat::RGBA8;
+
+    cvar pixelSize = Renderer::TextureFormatInfo[textureFormat][0];
+    cvar pitch = width * pixelSize / 8;
+    cvar textureSize = height * pitch;
+
+    m_format = textureFormat;
+    m_width = width;
+    m_height = height;
+    m_pitch = pitch;
+
+    m_textureMemory = Renderer::Allocate(textureSize, 0);
+    m_textureMemorySize = textureSize;
+}
+
+void Texture::InitializeFromMemory(uint8_t* data, const size_t dataSize, const uint16_t width, const uint16_t height, const uint8_t mipNum, 
+    const Renderer::TextureFormat::_enum textureFormat)
+{
+    cvar pixelSize = Renderer::TextureFormatInfo[textureFormat][0];
+    cvar pitch = width * pixelSize / 8;
+
+    m_format = textureFormat;
+    m_width = width;
+    m_height = height;
+    m_pitch = pitch;
+    m_mips = mipNum;
+
+    m_textureMemory = Renderer::Allocate(dataSize, 0);
+    m_textureMemorySize = dataSize;
+
+    memcpy(m_textureMemory, data, dataSize);
+}
+
+
+uint Texture::GetPixel(const int x, const int y) const
+{
+    ASSERT(m_applied);
+    _ASSERT_(m_textureMemory, "Cannot get pixel from this texture. Texture not initialized.");
+    _ASSERT_(m_textureFormat == Renderer::TextureFormat::RGBA8, "Cannot get pixel from this texture. Invalid texture format. Must be RGBA8.");
+
+    cvar bits = static_cast<uint8_t*>(m_textureMemory);
+    cvar idx = y * m_width + x;
+
+    cvar r = bits[idx + 0];
+    cvar g = bits[idx + 1];
+    cvar b = bits[idx + 2];
+    cvar a = bits[idx + 3];
+
+    return a | b << 8 | g << 16 | r << 24;
+}
+
+uint* Texture::GetPixels() const
+{
+    ASSERT(m_applied);
+    _ASSERT_(m_textureMemory, "Cannot get pixel from this texture. Texture not initialized.");
+
+    return static_cast<uint*>(m_textureMemory);
+}
+
+void Texture::SetPixel(const int x, const int y, const uint pixel) const
+{
+    ASSERT(m_applied);
+    _ASSERT_(m_textureMemory, "Cannot get pixel from this texture. Texture not initialized.");
+    _ASSERT_(m_textureFormat == Renderer::TextureFormat::RGBA8, "Cannot get pixel from this texture. Invalid texture format. Must be RGBA8.");
+    
+    cvar bits = static_cast<uint8_t*>(m_textureMemory);
+
+    cvar idx = y * m_width + x;
+    cvar pixels = reinterpret_cast<uint*>(bits);
+    pixels[idx] = pixel;
+}
+
+void Texture::SetPixel(const int x, const int y, const byte r, const byte g, const byte b, const byte a) const
+{
+    ASSERT(m_applied);
+    _ASSERT_(m_textureMemory, "Cannot get pixel from this texture. Texture not initialized.");
+    _ASSERT_(m_textureFormat == Renderer::TextureFormat::RGBA8, "Cannot get pixel from this texture. Invalid texture format. Must be RGBA8.");
+
+    cvar bits = static_cast<uint8_t*>(m_textureMemory);
+
+    cvar idx = y * m_width + x;
+    cvar pixels = reinterpret_cast<uint*>(bits);
+    pixels[idx] = a | b << 8 | g << 16 | r << 24;
+}
+
+void Texture::SetPixels(const int x, const int y, const int width, const int height, uint* pixels) const
+{
+    ASSERT(m_applied);
+    _ASSERT_(m_textureMemory, "Cannot get pixel from this texture. Texture not initialized.");
+
+    cvar bits = static_cast<uint8_t*>(m_textureMemory);
+    for (auto my = y; my < y + height; my++)
+    {
+        for (auto mx = x; mx < x + width; mx++)
+        {
+            bits[my * m_width + mx] = *pixels++;
+        }
+    }
+}
+
+void Texture::Apply(const bool generateMips)
+{
+    ASSERT(m_textureMemory);
+    ASSERT(m_width > 0);
+    ASSERT(m_height > 0);
+    ASSERT(m_pitch > 0);
+
+    // Convert dxgi format to texture format
+    m_textureFormat = Renderer::DXGIFormatToTextureFormat(m_format);
+
+    // Create texture
+    m_textureHandle = Renderer::CreateTexture2D(m_width, m_height, m_pitch, m_mips, m_textureFormat, m_textureMemory, m_textureMemorySize, false, generateMips);
+    Renderer::QueueFree(m_textureMemory);
+
+    m_textureMemory = nullptr;
+    m_applied = true;
 }
--- src/ReCrafted.Engine/Graphics/Texture.h
+++ src/ReCrafted.Engine/Graphics/Texture.h
@@ -10,15 +10,21 @@
 #include "Content/Assets/BinaryAsset.h"
 #include "Renderer/Renderer.hpp"
 
-class Texture : BinaryAsset
+class Texture : public BinaryAsset
 {
 private:
     Renderer::Texture2DHandle m_textureHandle;
+    Renderer::RendererMemory m_textureMemory = nullptr;
+    size_t m_textureMemorySize = 0u;
+    Renderer::TextureFormat::_enum m_textureFormat = Renderer::TextureFormat::RGBA8;
 
     uint16_t m_width = 0u;
     uint16_t m_height = 0u;
     uint16_t m_format = 0u;
+    uint16_t m_pitch = 0u;
     uint8_t m_mips = 0u;
+
+    bool m_applied = false;
 
 protected:
     void OnInitialize() override;
@@ -28,11 +34,103 @@ protected:
     
     AssetType GetAssetType() override
     {
-        return AssetType::Texture;
+        return AssetType::Texture2D;
     }
 
 public:
-    void LoadFromMemory(uint8_t* data, uint16_t width, uint16_t height, uint8_t mips, uint8_t bitsPerPixel = 32);
+    /**
+     * \brief Initializes this texture and allocates memory using texture format RGBA8.
+     * \param width The texture width.
+     * \param height The texture height.
+     */
+    void Initialize(uint16_t width, uint16_t height);
+
+    /**
+     * \brief Initializes this texture from given memory pointer. Data gets copied into allocated texture memory.
+     * \param data The data pointer.
+     * \param dataSize The data size.
+     * \param width The texture width.
+     * \param height The texture height.
+     * \param mipNum The amount of mip maps.
+     * \param textureFormat The texture format.
+     * 
+     * \note Requires Apply call.
+     */
+    void InitializeFromMemory(uint8_t* data, size_t dataSize, uint16_t width, uint16_t height, uint8_t mipNum,
+                              Renderer::TextureFormat::_enum textureFormat);
+
+    /**
+     * \brief Get pixel at given position.
+     * \param x The x coord.
+     * \param y The y coord.
+     * \return Pixel HEX format.
+     * 
+     * \note Available only before Apply call, when format is RGBA8 
+     * and has been initialized (Texture::Initialize(...)).
+     */
+    uint GetPixel(int x, int y) const;
+
+    /**
+     * \brief Get all pixels in the texture(Returns pixel buffer).
+     * \return The pixels data.
+     *
+     * \note Available only before Apply call.
+     */
+    uint* GetPixels() const;
+
+    /**
+     * \brief Sets pixel at given position.
+     * \param x The x coord.
+     * \param y The y coord.
+     * \param pixel Pixel in HEX format.
+     * 
+     * \note Available only before Apply call, when format is RGBA8 
+     * and has been initialized (Texture::Initialize(...)).
+     */
+    void SetPixel(int x, int y, uint pixel) const;
+
+    /**
+    * \brief Sets pixel at given position.
+    * \param x The x coord.
+    * \param y The y coord.
+    * \param r R channel value.
+    * \param g G channel value.
+    * \param b B channel value.
+    * \param a A channel value.
+    * 
+     * \note Available only before Apply call, when format is RGBA8 
+     * and has been initialized (Texture::Initialize(...)).
+    */
+    void SetPixel(int x, int y, byte r, byte g, byte b, byte a) const;
+
+    /**
+     * \brief Sets pixels at given position with given size.
+     * \param x The start X coord.
+     * \param y The start Y coord.
+     * \param width The width of the pixel array.
+     * \param height The height of the pixel array.
+     * \param pixels The pixels data.
+     * 
+     * \note Available only before Apply call.
+     */
+    void SetPixels(int x, int y, int width, int height, uint* pixels) const;
+
+    /**
+     * \brief Apply, all pixel operations will be unavailable after calling this method.
+     * \param generateMips When true, this texture will get online-generated mip maps.
+     */
+    void Apply(bool generateMips = false);
+
+public:
+    /**
+     * \brief Gets handle of this texture.
+     * \return G
+     */
+    Renderer::Texture2DHandle GetHandle() const
+    {
+        ASSERT(RENDERER_CHECK_HANDLE(m_textureHandle));
+        return m_textureHandle;
+    }
 };
 
 #endif // TEXTURE_H
