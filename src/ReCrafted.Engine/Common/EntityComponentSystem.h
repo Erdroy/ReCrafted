51b0d55700357f4164d4a0d8dde06ef0854e9bc9
Damian Korczowski <erdroy2@gmail.com>
2018-10-11 17:30:47 +0200

    Added ECS component id initialization

--- src/ReCrafted.Engine/Common/EntityComponentSystem.h
+++ src/ReCrafted.Engine/Common/EntityComponentSystem.h
@@ -13,5 +13,25 @@
 // Default components
 #include "Common/TransformComponent.h"
 #include "Scripting/ScriptingComponent.h"
+#include "Audio/AudioSoundComponent.h"
+
+#include <cassert>
+
+class ECS
+{
+public:
+    ECS() {
+        // NOTE: Here you should add all of the native components,
+        // this helps to make sure, that .NET and Native components 
+        // have the same ComponentId.
+
+        // NOTE: None of the engine components aren't initialized, so we cannot
+        // use our ASSERT etc.
+
+        /* 00 */ assert(GetComponentId<TransformComponent>() == 0);
+        /* 01 */ assert(GetComponentId<ScriptingComponent>() == 1);
+        /* 02 */ assert(GetComponentId<AudioSoundComponent>() == 2);
+    }
+};
 
 #endif // ENTITYCOMPONENTSYSTEM_H
--- src/ReCrafted.Engine/ReCrafted.cpp
+++ src/ReCrafted.Engine/ReCrafted.cpp
@@ -1,3 +1,7 @@
 // ReCrafted (c) 2016-2018 Always Too Late
 
 #include "ReCrafted.h"
+
+// Initialize ECS
+#include "Common/EntityComponentSystem.h"
+static ECS ECSInstance;
