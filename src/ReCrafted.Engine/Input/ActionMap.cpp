e7cf4f95a206a0946ca310a9a937886147b9436f
Damian Korczowski <erdroy2@gmail.com>
2018-12-22 18:33:39 +0100

    Added ActionMap actions, bindings and listeners

--- src/ReCrafted.Engine/Input/ActionMap.cpp
+++ src/ReCrafted.Engine/Input/ActionMap.cpp
@@ -1,7 +1,150 @@
 // ReCrafted (c) 2016-2018 Always Too Late
 
 #include "ActionMap.h"
+#include "InputManager.h"
+
+ActionMap::ActionItem* ActionMap::GetActionItem(const char* name)
+{
+    ScopeLock(m_actionItemsLock);
+
+    crvar it = m_actionItemMap.find(std::string(name));
+    if (it == m_actionItemMap.end())
+        return nullptr;
+    return &it->second;
+}
 
 void ActionMap::Update()
 {
+    if (!m_active)
+        return;
+
+    m_actionItemsLock.LockNow();
+    for (rvar item : m_actionItemMap)
+    {
+        var actionItem = &item.second;
+
+        // We need to make something like this:
+        // if(InputManager::IsButton(actionItem->buttonBinds[0]))
+        //    actionItem->eventListeners[0].Invoke();
+        // But a lot faster, so we are going to need the generic input buffer (but how?)
+        // Maybe just go ahead, and make it per-frame input buffer, and let devices and maps
+        // do what they want?
+
+        // TODO: Event implementation
+        // TODO: State implementation
+        // TODO: Axis implementation
+    }
+    m_actionItemsLock.UnlockNow();
+}
+
+void ActionMap::SetActive(const bool active)
+{
+    m_active = active;
+}
+
+bool ActionMap::IsActive() const
+{
+    return m_active;
+}
+
+void ActionMap::AddAction(const char* name, const ActionType actionType, const ActionEventType eventType)
+{
+    ScopeLock(m_actionItemsLock);
+
+    ActionItem item;
+    item.type = actionType;
+    item.eventType = eventType;
+    item.name = name;
+
+    // Insert new action item
+    m_actionItemMap.insert(std::make_pair(std::string(name), item));
+}
+
+void ActionMap::RemoveAction(const char* name)
+{
+    ScopeLock(m_actionItemsLock);
+
+    // Find and remove action item if found
+    cvar it = m_actionItemMap.find(std::string(name));
+    if (it != m_actionItemMap.end())
+        m_actionItemMap.erase(it);
+}
+
+void ActionMap::AddBind(const char* name, const Key key)
+{
+    cvar action = GetActionItem(name);
+
+    // Add key bind
+    if (action)
+        action->keyBinds.push_back(key);
+}
+
+void ActionMap::AddBind(const char* name, const Button button)
+{
+    cvar action = GetActionItem(name);
+
+    // Add button bind
+    if (action)
+        action->buttonBinds.push_back(button);
+}
+
+void ActionMap::RemoveBinds(const char* name)
+{
+    cvar action = GetActionItem(name);
+    if (!action)
+        return;
+
+    // Clear key and button binds
+    action->keyBinds.clear();
+    action->buttonBinds.clear();
+}
+
+void ActionMap::AddListener(const char* name, const Action<void>& action)
+{
+    cvar actionItem = GetActionItem(name);
+    if (!actionItem)
+        return;
+
+    // Add listener
+    actionItem->eventListeners.push_back(action);
+}
+
+void ActionMap::AddListener(const char* name, const Action<void, InputState>& action)
+{
+    cvar actionItem = GetActionItem(name);
+    if (!actionItem)
+        return;
+
+    // Add listener
+    actionItem->stateListeners.push_back(action);
+}
+
+void ActionMap::AddListener(const char* name, const Action<void, float>& action)
+{
+    cvar actionItem = GetActionItem(name);
+    if (!actionItem)
+        return;
+
+    // Add listener
+    actionItem->axis1DListeners.push_back(action);
+}
+
+void ActionMap::AddListener(const char* name, const Action<void, Vector2>& action)
+{
+    cvar actionItem = GetActionItem(name);
+    if (!actionItem)
+        return;
+
+    // Add listener
+    actionItem->axis2DListeners.push_back(action);
+}
+
+void ActionMap::AddListener(const char* name, const Action<void, Vector3>& action)
+{
+    cvar actionItem = GetActionItem(name);
+    if (!actionItem)
+        return;
+
+    // Add listener
+    actionItem->axis3DListeners.push_back(action);
 }
--- src/ReCrafted.Engine/Input/ActionMap.h
+++ src/ReCrafted.Engine/Input/ActionMap.h
@@ -6,17 +6,82 @@
 #define ACTIONMAP_H
 
 #include "ReCrafted.h"
+#include "Core/Action.h"
+#include "Core/Lock.h"
+#include "Input/KeyboardKeys.h"
+#include "Input/MouseButtons.h"
 #include "Scripting/Object.h"
+
+enum class AxisFiltering : byte
+{
+    /**
+     * \brief No filtering for action.
+     */
+    None,
+
+    /**
+     * \brief Use a number of previous frames to filter-out the input.
+     */
+    OverFrames
+};
+
+enum class ActionType : byte
+{
+    Event,
+
+    State,
+
+    Axis1D,
+    Axis2D,
+    Axis3D
+};
+
+enum class ActionEventType : byte
+{
+    None,
+    Pressed = 1 << 0,
+    Released = 1 << 1,
+    Held = 1 << 2
+};
+
+struct InputState
+{
+    bool pressed : 1;
+    bool held : 1;
+    bool released : 1;
+};
+static_assert(sizeof(InputState), "InputState structure size must be 1 byte long");
 
 class ActionMap : public Object
 {
     friend class InputManager;
 
 private:
+    struct ActionItem
+    {
+        std::string name;
+        ActionType type;
+        ActionEventType eventType;
+
+        std::vector<Key> keyBinds;
+        std::vector<Button> buttonBinds;
+
+        std::vector<Action<void>> eventListeners;
+        std::vector<Action<void, InputState>> stateListeners;
+        std::vector<Action<void, float>> axis1DListeners;
+        std::vector<Action<void, Vector2>> axis2DListeners;
+        std::vector<Action<void, Vector3>> axis3DListeners;
+    };
+
+private:
     SCRIPTING_API_IMPL();
 
 private:
+    bool m_active = true;
     std::string m_name;
+
+    spp::sparse_hash_map<std::string, ActionItem> m_actionItemMap;
+    Lock m_actionItemsLock;
 
 private:
     void SetName(const std::string& name)
@@ -28,6 +93,8 @@ private:
     {
         return m_name;
     }
+
+    ActionItem* GetActionItem(const char* name);
 
 public:
     ActionMap() = default;
@@ -47,9 +114,28 @@ protected:
     }
 
 public:
-
+    void SetActive(bool active);
+    bool IsActive() const;
 
 public:
+    void AddAction(const char* name, ActionType actionType, ActionEventType eventType = ActionEventType::None);
+    void RemoveAction(const char* name);
+
+public:
+    void AddBind(const char* name, Key key);
+    void AddBind(const char* name, Button button);
+    void RemoveBinds(const char* name);
+
+public:
+    void AddListener(const char* name, const Action<void>& action);
+    void AddListener(const char* name, const Action<void, InputState>& action);
+    void AddListener(const char* name, const Action<void, float>& action);
+    void AddListener(const char* name, const Action<void, Vector2>& action);
+    void AddListener(const char* name, const Action<void, Vector3>& action);
+
+public:
+    bool operator[](const char* name) const {} // TODO: Manual listening implementation
+    
     ActionMap& operator=(const ActionMap& other) = delete;
 };
 
