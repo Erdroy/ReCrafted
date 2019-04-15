dd785056aa4eb0078284096e02bd74f896792f5a
Damian Korczowski <erdroy2@gmail.com>
2019-04-15 20:11:36 +0200

    Improved Singletion implementation (Added possibility do Dispose singleton's object)

--- Source/Engine/ReCrafted.Common/Common/Singleton.h
+++ Source/Engine/ReCrafted.Common/Common/Singleton.h
@@ -4,21 +4,58 @@
 
 /**
  * \brief Singleton class.
- * \tparam T The singleton's handled object type
+ * \tparam TType The singleton's handled object type
  *
- * \note PLEASE remember to add SINGLETON_IMPL(NAME_OF_CLASS) to the source file!
- * To release a Singleton, please call 'Dispose' function.
+ * \note To release a Singleton, please call 'Dispose' function 
+ * \remarks Dispose calls the object's destructor and deletes it from the memory.
  */
-template <class T>
+template <class TType>
 class Singleton
 {
 public:
+    Singleton() = default;
     virtual ~Singleton() = default;
 
-public:
-    static T* GetInstance()
+private:
+    static TType*& GetInstanceRef()
     {
-        static T instance;
-        return &instance;
+#if DEBUG
+        static auto* instance = reinterpret_cast<TType*>(0x1);
+
+        if (instance == reinterpret_cast<TType*>(0x1))
+            instance = new TType();
+
+        if (instance == nullptr)
+            throw std::exception("");
+#else
+        static auto* instance;
+
+        if (instance == nullptr)
+            instance = new TType();
+#endif
+
+        return instance;
+    }
+
+public:
+    /**
+     * \brief Disposes the singleton object.
+     * Next call to GetInstance will throw an exception.
+     */
+    void Dispose() const
+    {
+        auto& instance = GetInstanceRef();
+        delete instance;
+        instance = nullptr;
+    }
+
+public:
+    /**
+     * \brief Gets (or creates) the singleton object.
+     * \return The singleton object of type `TType`.
+     */
+    static TType* GetInstance()
+    {
+        return GetInstanceRef();
     }
 };
