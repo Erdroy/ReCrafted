b565c6023c8bd62df3c2dab8eb0f0990abd215bf
Damian Korczowski <erdroy2@gmail.com>
2019-05-01 13:54:23 +0200

    Removed API testing class code.

--- Source/Engine/ReCrafted.Engine/Core/TestClass.Gen.cpp
+++
@@ -1,4 +0,0 @@
-// ReCrafted (c) 2016-2019 Damian 'Erdroy' Korczowski. All rights reserved.
-// WARNING: Auto-generated file. All changes will be lost when the API code will be regenerated!
-
-#include "TestClass.h"
