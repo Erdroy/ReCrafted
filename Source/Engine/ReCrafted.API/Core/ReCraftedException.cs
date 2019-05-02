a5be8e3d866faa92e219c3bfb01d0bbbdfe2f4e1
Damian Korczowski <erdroy2@gmail.com>
2019-05-02 18:15:49 +0200

    Added ReCraftedException

---
+++ Source/Engine/ReCrafted.API/Core/ReCraftedException.cs
@@ -0,0 +1,14 @@
+﻿// ReCrafted (c) 2016-2019 Damian 'Erdroy' Korczowski. All rights reserved.
+
+using System;
+
+namespace ReCrafted.API.Core
+{
+    [Serializable]
+    public class ReCraftedException : Exception
+    {
+        public ReCraftedException(string message) : base(message)
+        {
+        }
+    }
+}
