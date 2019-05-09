59241c0059a0dfb9bfa2bed532e94f08b718c314
Damian Korczowski <erdroy2@gmail.com>
2019-05-09 17:07:54 +0200

    Added additional project which contains Game and API c# projects (for mono debugging purpose).

--- premake5.lua
+++ premake5.lua
@@ -16,39 +16,46 @@ solution "ReCrafted"
 		"Development",
 		"Release",
 	}
+
+	-- Project scripts
+	group "Libraries"
+		dofile "./Source/Libraries/Project.concurrentqueue.lua"
+		dofile "./Source/Libraries/Project.cpp-base64.lua"
+		dofile "./Source/Libraries/Project.fmt.lua"
+		dofile "./Source/Libraries/Project.json.lua"
+		dofile "./Source/Libraries/Project.sparsepp.lua"
+
+	group "Engine"
+		dofile "./Source/Engine/ReCrafted.API/Project.lua"
+		dofile "./Source/Engine/ReCrafted.Engine/Project.lua"
+
+	group "Game"
+		dofile "./Source/Game/ReCrafted/Project.lua"
+		dofile "./Source/Game/ReCrafted.Game/Project.lua"
+
+	group "Tools"
+		dofile "./Source/Tools/ReCrafted.VSDebugger/Project.lua"
+		dofile "./Source/Tools/ReCrafted.Tools.Common/Project.lua"
+		dofile "./Source/Tools/ReCrafted.Tools.APIGenerator/Project.lua"
+		dofile "./Source/Tools/ReCrafted.Tools.ShaderCompiler/Project.lua"
+		dofile "./Source/Tools/ReCrafted.Tools.ProjectManager/Project.lua"
+		dofile "./Source/Tools/ReCrafted.Tools.LocaleConverter/Project.lua"
+		dofile "./Source/Tools/ReCrafted.Tools.AtlasPacker/Project.lua"
+
+-- generate C# Game/API only solution
+solution "ReCrafted.Game"
+	location (SOURCE_DIR)
+	startproject "ReCrafted.Game"
+
+	configurations {
+		"Debug",
+		"Development",
+		"Release",
+	}
 	
-	-- define platforms
-	if _ACTION == "xcode4" then
-		platforms {
-			"Universal",
-		}
-	else
-		platforms {
-			"x64"
-		}
-	end
-
--- Project scripts
-group "Libraries"
-dofile "./Source/Libraries/Project.concurrentqueue.lua"
-dofile "./Source/Libraries/Project.cpp-base64.lua"
-dofile "./Source/Libraries/Project.fmt.lua"
-dofile "./Source/Libraries/Project.json.lua"
-dofile "./Source/Libraries/Project.sparsepp.lua"
-
-group "Engine"
-dofile "./Source/Engine/ReCrafted.API/Project.lua"
-dofile "./Source/Engine/ReCrafted.Engine/Project.lua"
-
-group "Game"
-dofile "./Source/Game/ReCrafted/Project.lua"
-dofile "./Source/Game/ReCrafted.Game/Project.lua"
-
-group "Tools"
-dofile "./Source/Tools/ReCrafted.VSDebugger/Project.lua"
-dofile "./Source/Tools/ReCrafted.Tools.Common/Project.lua"
-dofile "./Source/Tools/ReCrafted.Tools.APIGenerator/Project.lua"
-dofile "./Source/Tools/ReCrafted.Tools.ShaderCompiler/Project.lua"
-dofile "./Source/Tools/ReCrafted.Tools.ProjectManager/Project.lua"
-dofile "./Source/Tools/ReCrafted.Tools.LocaleConverter/Project.lua"
-dofile "./Source/Tools/ReCrafted.Tools.AtlasPacker/Project.lua"
+	group "Engine"
+		dofile "./Source/Engine/ReCrafted.API/Project.lua"
+	
+	group "Game"
+		dofile "./Source/Game/ReCrafted.Game/Project.lua"
+	
