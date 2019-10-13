8c3bc3abd92c3514697518c47fdf920131a74840
Damian Korczowski <erdroy2@gmail.com>
2019-10-13 13:15:37 +0200

    Added AssetFinder dialog window

---
+++ Source/Tools/ReCrafted.Tools.ContentEditor/Content/Common/AssetFinder.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ReCrafted.Tools.ContentEditor.Content.Common
+{
+    partial class AssetFinder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ContentView = new System.Windows.Forms.ListView();
+            this.AssetFilter = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ContentView
+            // 
+            this.ContentView.AllowDrop = true;
+            this.ContentView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ContentView.AutoArrange = false;
+            this.ContentView.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ContentView.HideSelection = false;
+            this.ContentView.LabelEdit = true;
+            this.ContentView.Location = new System.Drawing.Point(12, 38);
+            this.ContentView.Name = "ContentView";
+            this.ContentView.Size = new System.Drawing.Size(620, 391);
+            this.ContentView.TabIndex = 1;
+            this.ContentView.TileSize = new System.Drawing.Size(256, 60);
+            this.ContentView.UseCompatibleStateImageBehavior = false;
+            this.ContentView.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.ContentView_MouseDoubleClick);
+            // 
+            // AssetFilter
+            // 
+            this.AssetFilter.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.AssetFilter.Location = new System.Drawing.Point(12, 12);
+            this.AssetFilter.Name = "AssetFilter";
+            this.AssetFilter.Size = new System.Drawing.Size(620, 20);
+            this.AssetFilter.TabIndex = 2;
+            this.AssetFilter.Text = "t:*; n:*";
+            this.AssetFilter.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // AssetFinder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 441);
+            this.Controls.Add(this.AssetFilter);
+            this.Controls.Add(this.ContentView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "AssetFinder";
+            this.Text = "Asset Finder";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.AssetFinder_FormClosing);
+            this.Load += new System.EventHandler(this.AssetFinder_Load);
+            this.ResizeEnd += new System.EventHandler(this.AssetFinder_Resized);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView ContentView;
+        private System.Windows.Forms.TextBox AssetFilter;
+    }
+}
---
+++ Source/Tools/ReCrafted.Tools.ContentEditor/Content/Common/AssetFinder.cs
@@ -0,0 +1,147 @@
+﻿// ReCrafted (c) 2016-2019 Damian 'Erdroy' Korczowski. All rights reserved.
+
+using System;
+using System.Collections;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using ReCrafted.Tools.ContentEditor.Properties;
+
+namespace ReCrafted.Tools.ContentEditor.Content.Common
+{
+    public partial class AssetFinder : Form
+    {
+        public const int MaxItems = 64;
+
+        public AssetFinder()
+        {
+            CurrentFilter = "t:*;n:*";
+            InitializeComponent();
+        }
+
+        private void AssetFinder_Load(object sender, System.EventArgs e)
+        {
+            AssetFilter.Text = CurrentFilter;
+            SetupContentView();
+            RefreshAssets();
+        }
+
+        private void AssetFinder_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (DialogResult != DialogResult.OK)
+                DialogResult = DialogResult.Abort;
+        }
+
+        private void AssetFinder_Resized(object sender, System.EventArgs e)
+        {
+            RefreshAssets();
+        }
+
+        private void TextBox1_TextChanged(object sender, System.EventArgs e)
+        {
+            CurrentFilter = AssetFilter.Text;
+            RefreshAssets();
+        }
+
+        private void ContentView_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            var senderList = (ListView)sender;
+            var clickedItem = senderList.HitTest(e.Location).Item;
+
+            if (clickedItem != null)
+            {
+                if (clickedItem.Tag is AssetCache.AssetCacheItem asset)
+                {
+                    SelectedAsset = asset;
+                    DialogResult = DialogResult.OK;
+                    Close();
+                }
+            }
+        }
+
+        private void RefreshAssets()
+        {
+            var filterParts = CurrentFilter.Split(new[] {";", ","}, StringSplitOptions.RemoveEmptyEntries);
+
+            var nameFilter = filterParts.FirstOrDefault(x => x.StartsWith("n:", StringComparison.InvariantCultureIgnoreCase));
+            if (!string.IsNullOrEmpty(nameFilter))
+                nameFilter = nameFilter.Replace("n:", "");
+
+            var typeFilter = filterParts.FirstOrDefault(x => x.StartsWith("t:", StringComparison.InvariantCultureIgnoreCase));
+            if (!string.IsNullOrEmpty(typeFilter))
+                typeFilter = typeFilter.Replace("t:", "");
+
+            ContentView.Items.Clear();
+
+            var i = 0;
+            foreach (var asset in AssetCache.Assets)
+            {
+                if (i >= MaxItems)
+                    break;
+
+                var assetName = Path.GetFileNameWithoutExtension(asset.AssetFile);
+                var assetType = asset.AssetType.ToString();
+
+                if (assetName != null && !string.IsNullOrEmpty(nameFilter) && nameFilter != "*")
+                {
+                    if (assetName.IndexOf(nameFilter, StringComparison.InvariantCultureIgnoreCase) == -1)
+                        continue;
+                }
+
+                if (string.IsNullOrEmpty(ForceType))
+                {
+                    if (!string.IsNullOrEmpty(typeFilter) && typeFilter != "*")
+                    {
+                        if (assetType.IndexOf(typeFilter, StringComparison.InvariantCultureIgnoreCase) == -1)
+                            continue;
+                    }
+                }
+                else
+                {
+                    if (assetType.IndexOf(ForceType, StringComparison.InvariantCultureIgnoreCase) == -1)
+                        continue;
+                }
+
+                ContentView.Items.Add(new ListViewItem
+                {
+                    Text = assetName,
+                    Tag = asset,
+                    ImageIndex = 0 // TODO: Generate preview
+                });
+
+                i++;
+            }
+        }
+
+        private void SetupContentView()
+        {
+            ContentViewImages = new ImageList
+            {
+                ImageSize = new Size(64, 64),
+                ColorDepth = ColorDepth.Depth32Bit
+            };
+
+            var resourceSet = Resources.ResourceManager.GetResourceSet(CultureInfo.InvariantCulture, true, false);
+            if (resourceSet != null)
+            {
+                foreach (DictionaryEntry entry in resourceSet)
+                {
+                    if (entry.Value is Bitmap value)
+                    {
+                        ContentViewImages.Images.Add((string)entry.Key, value);
+                    }
+                }
+            }
+
+            // Setup image lists
+            ContentView.LargeImageList = ContentViewImages;
+        }
+
+        public AssetCache.AssetCacheItem SelectedAsset { get; private set; }
+        public string CurrentFilter { get; set; }
+        public string ForceType { get; set; }
+        public ImageList ContentViewImages { get; set; }
+    }
+}
