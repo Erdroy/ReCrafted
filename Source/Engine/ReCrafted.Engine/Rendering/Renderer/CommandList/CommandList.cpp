d0c5e1546b2ed93a75aea8cb99ac51c556b95270
Damian Korczowski <erdroy2@gmail.com>
2019-11-18 20:12:24 +0100

    Cleanup

--- Source/Engine/ReCrafted.Engine/Rendering/Renderer/CommandList/CommandList.cpp
+++ Source/Engine/ReCrafted.Engine/Rendering/Renderer/CommandList/CommandList.cpp
@@ -18,59 +18,15 @@ namespace Renderer
         ASSERT(dataBegin);
         ASSERT(commandCount);
 
-        //if(m_commandCount < RENDERER_COMMAND_LIST_MIN_CMD_COUNT || threadCount == 1)
+        for (auto i = 0u; i < threadCount; i++)
         {
-            for (auto i = 0u; i < threadCount; i++)
-            {
-                dataBegin[i] = 0;
-                commandCount[i] = 0;
-            }
-
-            dataBegin[0] = 0;
-            dataEnd[0] = size;
-            commandCount[0] = m_commandCount;
-            //return;
+            dataBegin[i] = 0;
+            commandCount[i] = 0;
         }
 
-        /*const auto commandsPerThread = m_commandCount / threadCount;
-        var commands = 0u;
-        var totalSize = 0u;
-        var thread = 0u;
-
-        var position = 0u;
-
-        dataBegin[thread] = 0;
-
-        for(var i = 0u; i < m_commandCount; i ++)
-        {
-            auto spos = position;
-            Read_CommandHeader(&position);
-            auto size = Read_uint16(&position);
-            position = spos + size;
-            commands++;
-            totalSize += size;
-
-            // Read till command count is >= commandsPerThread - write command count,
-            // if we filled all allowed commands, increment thread, write data start and continue
-
-            if(commands >= commandsPerThread)
-            {
-                commandCount[thread] = commands;
-
-                if(thread + 1 >= threadCount)
-                {
-                    ASSERT(i < m_commandCount);
-                    break;
-                }
-
-                thread++;
-
-                dataBegin[thread] = position;
-                dataEnd[thread] = position + totalSize;
-                commands = 0u;
-                totalSize = 0;
-            }
-        }*/
+        dataBegin[0] = 0;
+        dataEnd[0] = size;
+        commandCount[0] = m_commandCount;
     }
 
     void CommandList::Destroy()
